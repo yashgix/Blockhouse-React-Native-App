c6abb552fcef909877274323a0b533830b23e491
yashgix <yashg.gupta0609@gmail.com>
2025-01-26 01:08:02 -0500

    Update comment in LoginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -22,6 +22,7 @@
     //.email('Invalid email')
     .required('Email is required'),
   password: Yup.string()
+    //.min(8, 'Password must be at least 8 characters')
     .required('Password is required')
   });
   
